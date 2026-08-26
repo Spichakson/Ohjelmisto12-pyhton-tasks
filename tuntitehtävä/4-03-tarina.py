249d540fea0db829e65d6ac707f6e7507bb61660
Artem Spichak <artemspichak@gmail.com>
2026-08-26 14:29:14 +0300

    26.8 3

--- tuntitehtävä/4-03-tarina.py
+++ tuntitehtävä/4-03-tarina.py
@@ -1,13 +1,9 @@
+print("Olle on iso mies töissä jätskikioskissa."
+" Hähen lempiruoka on kirjolohi, ja hän syö sitä kesällä ja ei anna kennellekaan."
+" Eräänä paivänä hänen luokse tuli Elina volkkariilla. Elinan lempijäätelö oli lakriitsijäätelö. "
+f"Ne söivät jätskiä yhdessä. Olle kysyi Elinaa :Haluatko mennä rannalle?: " )
 
-
-elinaDecision = input('Haluatko mennä rannalle?: ')
-if elinaDecision == 'Joo':
-    print("Elina sanoi 'Joo' ja he lähtivät kävelemaan rannalle")
-
-    
-
-
-
-
-
+decision = input('Haluatko mennä rannalle?: ')
+if decision == 'joo' or decision == 'kyllä':
+    print("Elina sanoi 'Joo' ja he lähtivät kävelemaan rannalle.")
 
