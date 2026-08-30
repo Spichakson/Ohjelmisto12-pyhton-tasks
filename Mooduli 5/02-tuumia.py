6b9603756edf916d811ad6f5d46a1e194fe06f5c
Artem Spichak <artemspichak@gmail.com>
2026-08-31 02:14:54 +0300

    31.8/1

--- Mooduli 5/02-tuumia.py
+++ Mooduli 5/02-tuumia.py
@@ -1,6 +1,7 @@
 tuumia = int(input('Anna tuumat: '))
 while tuumia >= 0:
     print(f"{tuumia} tuumaa on {tuumia * 2.54} cm")
-    break
+    tuumia = int(input('Anna tuumat: '))
 else:
     print('Ei voi kirjoittaa negatiivista tuumamäärää!')
+    
