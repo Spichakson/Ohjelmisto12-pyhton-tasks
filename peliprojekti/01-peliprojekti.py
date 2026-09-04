3c612c4c86630c1466528f9af33f997c4f40b1a5
Artem Spichak <artemspichak@gmail.com>
2026-09-04 15:04:51 +0300

    4.9 final

--- peliprojekti/01-peliprojekti.py
+++ peliprojekti/01-peliprojekti.py
@@ -8,9 +8,20 @@ elif age >= 12:
     print(f'Ikä: {age}')
     print('Moi!')
 while True:
+    print('Käytä kommentolista')
     command = input('Anna kommento: ')
     if command == 'lopeta':
         print('Lopettu!')
         break
+    elif command == 'käyttäjä':
+        print(f'Nimi: {name}')
+        print(f'Ikä: {age}')
+    elif command == 'muoka nimi: ':
+        name = input('Anna uusi nimi')
+    elif command == 'kommentolista':
+        print('Komennot: ')
+        print(" 'käyttäjä' 'muoka nimi' 'lopeta' ")
+        
+
     
 
--- peliprojekti/readme.md
+++ peliprojekti/readme.md
@@ -1,3 +1,6 @@
 pelin nimi: Kotkan most wanted
-
 Artem Spichak
+
+# 4.9.2026
+
+Tein kaikki moodulin 5 tehtävät, opiskelin while-käyttämisen ja muokin peliprojektintehtävää
